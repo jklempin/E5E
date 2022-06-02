9eeeab6cbb1b663cc6d31d1803346849264734ae
JKlempin <JKlempin@anexia-it.com>
2022-06-02 11:34:29 +0200

    initial setup

---
+++ functions.js
@@ -0,0 +1,22 @@
+exports.cors = () => ({
+    status: 200,
+    response_headers: {
+        'Access-Control-Allow-Origin': '*',
+        'X-Frame-Options': 'SAMEORIGIN'
+    },
+    data: {}
+})
+exports.externalSource = () => ({
+    status: 200,
+    response_headers: {
+        //'Access-Control-Allow-Origin': '*',
+        //'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Type': 'application/json'
+    },
+    data: {
+        results: [
+            { id: 1, text: 'Item1' },
+            { id: 2, text: 'Item2' }
+        ]
+    }
+})

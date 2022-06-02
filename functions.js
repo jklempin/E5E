f02cb536659e22ce656c82ad2772bccf5c1a94fa
JKlempin <JKlempin@anexia-it.com>
2022-06-02 11:38:15 +0200

    change function

--- functions.js
+++ functions.js
@@ -6,17 +6,18 @@ exports.cors = () => ({
     },
     data: {}
 })
-exports.externalSource = () => ({
-    status: 200,
-    response_headers: {
-        //'Access-Control-Allow-Origin': '*',
-        //'X-Frame-Options': 'SAMEORIGIN',
-        'Content-Type': 'application/json'
-    },
-    data: {
-        results: [
-            { id: 1, text: 'Item1' },
-            { id: 2, text: 'Item2' }
-        ]
-    }
-})
+exports.externalSource = () => {
+    return ({
+        status: 200,
+        response_headers: {
+            //'Access-Control-Allow-Origin': '*',
+            //'X-Frame-Options': 'SAMEORIGIN',
+            'Content-Type': 'application/json'
+        },
+        data: {
+            results: [
+                {text: 'test'}
+            ]
+        }
+    });
+}

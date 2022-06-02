94e72b7a7f8d400e1915d9cf8cbda14153a3ba12
LTakacs <LTakacs@anexia-it.com>
2022-06-02 12:15:51 +0200

    feat: add dumpData function

--- functions.js
+++ functions.js
@@ -1,23 +1,34 @@
 exports.cors = () => ({
-    status: 200,
-    response_headers: {
-        'Access-Control-Allow-Origin': '*',
-        'X-Frame-Options': 'SAMEORIGIN'
-    },
-    data: {}
+  status: 200,
+  response_headers: {
+    'Access-Control-Allow-Origin': '*',
+    'X-Frame-Options': 'SAMEORIGIN'
+  },
+  data: {}
 })
 exports.externalSource = () => {
-    return ({
-        status: 200,
-        response_headers: {
-            //'Access-Control-Allow-Origin': '*',
-            //'X-Frame-Options': 'SAMEORIGIN',
-            'Content-Type': 'application/json'
-        },
-        data: {
-            results: [
-                {text: 'test'}
-            ]
-        }
-    });
+  return {
+    status: 200,
+    response_headers: {
+      //'Access-Control-Allow-Origin': '*',
+      //'X-Frame-Options': 'SAMEORIGIN',
+      'Content-Type': 'application/json'
+    },
+    data: {
+      results: [
+        {text: 'test'}
+      ]
+    }
+  };
+}
+exports.dumpData = (event) => {
+  return {
+    status: 200,
+    response_headers: {
+      'Content-Type': 'application/json'
+    },
+    data: {
+      input: event.data
+    }
+  };
 }
